dd5cd1527aea514a104ce3cc38133e4151c173e3
MadMarchHare <coding.and.playing@gmail.com>
2015-03-07 15:41:52 +0200

    v0.3

--- game.py
+++
@@ -1,9 +0,0 @@
-from core.core import Core
-from data.builder import Builder
-from core.shell import UserShell
-
-if __name__ == '__main__':
-    core = Core(Builder())
-    shell = UserShell()
-    shell.set_core(core)
-    shell.cmdloop()
